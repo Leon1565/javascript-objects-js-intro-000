5e2dea8b4b66606a2e6baab9b2a96c8d23264bc0
Cha Yang <chachaiyang3@gmail.com>
2019-07-21 21:03:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,21 +1,6 @@
 var playlist = {
-  artist1: "song1",
-  artist2: "song2",
-  artist3: "song3"
-};
-
-function updatePlaylist(thePlaylist, artistName, songTitle) {
-  return Object.assign({}, thePlaylist, { [artistName]: songTitle});
+  LukeCombs: "Beautiful Crazy"
 }
-
-updatePlaylist(playlist, "artist4", "song4");
-
-function removeFromPlaylist(thePlaylist, artistName) {
-  delete thePlaylist[artistName];
-  return thePlaylist;
+function updatePlaylist(){
+  
 }
-
-removeFromPlaylist(playlist, "artist2");
-
-
-
